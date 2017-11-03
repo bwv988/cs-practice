10573c76795255b970666c92f6649428761c78e7
Ralph Schlosser <ralph.schlosser@gmail.com>
2017-11-03 14:36:56 +0000

    Working but not accepted.

--- src/main/java/com/warpbreaks/io/hackerrank/DynamicArray.java
+++ src/main/java/com/warpbreaks/io/hackerrank/DynamicArray.java
@@ -1,60 +1,48 @@
 // Hackerrank: Dynamic Array Challenge.
 // RS03112017
 
-import java.io.*;
+
 import java.util.*;
-import java.text.*;
-import java.math.*;
-import java.util.regex.*;
 
 public class DynamicArray {
     
     public static void main(String[] args) {
         int N, Q, lastAnswer = 0;
-        List<List<Integer>> seqList = new ArrayList<ArrayList<Integer>>();
-        
+        ArrayList<ArrayList<Integer>> seqList = new ArrayList<ArrayList<Integer>>();
+
         Scanner console = new Scanner(System.in);
         Scanner lineTokenizer = new Scanner(console.nextLine());
-        
+
         // Read N and Q.
         // Not doing any checks here with hasNext() for now, assuming the input is OK.
-        N = lineTokenizer.nextInt(); 
+        N = lineTokenizer.nextInt();
         Q = lineTokenizer.nextInt();
-        
-        //System.out.println("Dimensions: " + N + ", " + Q);
-        
+
+        for(int i = 0; i < N; i++) {
+            seqList.add(new ArrayList<Integer>());
+        }
+
         // Can have at most Q inserts.
-        
         for(int i = 0; i < Q; i++) {
             lineTokenizer = new Scanner(console.nextLine());
-            
+
             int queryType = lineTokenizer.nextInt();
             int x = lineTokenizer.nextInt();
             int y = lineTokenizer.nextInt();
-            
+            int seq = (x ^ lastAnswer) % N;
+            ArrayList<Integer> sequence = seqList.get(seq);
+
             if (queryType == 1) {
-                int seqNum = (x ^ lastAnswer) % N;
-                System.out.println("seqNum = " + seqNum);
-                if(seqList.get(seqNum) == null) {
-                    seqList.get(seqNum) = new ArrayList<Integer>();
-                }
-                
-                seqList.get(seqNum).append(y);
-                
+                sequence.add(y);
             } else if (queryType == 2) {
-                
+                lastAnswer = sequence.get(y % sequence.size());
+                System.out.println(lastAnswer);
             }
-            
-            //System.out.println(queryType + " " + x + " " + y);
         }
         
         // Debug.
-        for (List<Integer> l: seqList) {
-            System.out.print(l + ": ");
-            for (int j: seqList.get(l)) {
-                System.out.print(j);
-            }
-            System.out.println("");
-        }
+        //for (ArrayList<Integer> sList: seqList) {
+        //System.out.println(sList);
+        //}
     }   
 }
