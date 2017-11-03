138d9678b06edb249700669157177acc876f6eea
Ralph Schlosser <ralph.schlosser@gmail.com>
2017-11-03 12:06:46 +0000

    Stuff...

--- src/main/java/com/warpbreaks/io/hackerrank/DynamicArray.java
+++ src/main/java/com/warpbreaks/io/hackerrank/DynamicArray.java
@@ -1,5 +1,5 @@
 // Hackerrank: Dynamic Array Challenge.
-
+// RS03112017
 
 import java.io.*;
 import java.util.*;
@@ -10,29 +10,51 @@ import java.util.regex.*;
 public class DynamicArray {
     
     public static void main(String[] args) {
-        int N, Q, lineNum = 0;
-        int [][] sequences;
+        int N, Q, lastAnswer = 0;
+        List<List<Integer>> seqList = new ArrayList<ArrayList<Integer>>();
+        
         Scanner console = new Scanner(System.in);
-        Scanner lineTokenizer new Scanner(console.nextLine());
+        Scanner lineTokenizer = new Scanner(console.nextLine());
         
         // Read N and Q.
         // Not doing any checks here with hasNext() for now, assuming the input is OK.
-        N = lineTokenizer.getInt(); 
-        Q = lineTokenizer.getInt();
+        N = lineTokenizer.nextInt(); 
+        Q = lineTokenizer.nextInt();
         
+        //System.out.println("Dimensions: " + N + ", " + Q);
         
-        while (console.hasNextLine()) {
+        // Can have at most Q inserts.
+        
+        for(int i = 0; i < Q; i++) {
             lineTokenizer = new Scanner(console.nextLine());
             
-            if (lineNum < 1)
-            // determine if the line has a name field
-            if (lineTokenizer.hasNext()) {
-	            lineTokenizer.next();  // consume the valid token
+            int queryType = lineTokenizer.nextInt();
+            int x = lineTokenizer.nextInt();
+            int y = lineTokenizer.nextInt();
+            
+            if (queryType == 1) {
+                int seqNum = (x ^ lastAnswer) % N;
+                System.out.println("seqNum = " + seqNum);
+                if(seqList.get(seqNum) == null) {
+                    seqList.get(seqNum) = new ArrayList<Integer>();
+                }
+                
+                seqList.get(seqNum).append(y);
+                
+            } else if (queryType == 2) {
+                
             }
             
-            lineNum++;
+            //System.out.println(queryType + " " + x + " " + y);
         }
-
-
+        
+        // Debug.
+        for (List<Integer> l: seqList) {
+            System.out.print(l + ": ");
+            for (int j: seqList.get(l)) {
+                System.out.print(j);
+            }
+            System.out.println("");
+        }
     }   
 }
